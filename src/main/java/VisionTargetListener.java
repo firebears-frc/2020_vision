f17719213b0171c382d4da0fca03528916f5f16c
gasongkiel <likeakiel@gmail.com>
2020-01-18 14:03:37 -0600

    geot width off of the vision target, and started working with the new main example file

--- src/main/java/VisionTargetListener.java
+++ src/main/java/VisionTargetListener.java
@@ -7,6 +7,7 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
@@ -35,12 +36,15 @@ public class VisionTargetListener implements VisionRunner.Listener<VisionTargetP
     public static final String TARGET_CONFIDENCE = "visionTarget.confidence";
 
     /** Number of vision target pairs */
-    public static final String TARGET_PAIRS = "visionTarget.pairs";
+    // public static final String TARGET_PAIRS = "visionTarget.pairs";
 
     /** Processing throughput in frames per second. */
     public static final String TARGET_FPS = "visionTarget.fps";
 
-    /** Milliseconds between saving image files.  A negative value indicates to not save. */
+    /**
+     * Milliseconds between saving image files. A negative value indicates to not
+     * save.
+     */
     public static final String TARGET_SAVE = "visionTarget.saveImageTime";
 
     /** Width of the best image pair, measured in pixels. */
@@ -81,59 +85,61 @@ public class VisionTargetListener implements VisionRunner.Listener<VisionTargetP
         double confidence = 0.0;
 
         // ArrayLists for left and right targets
-        ArrayList<MatOfPoint> leftTargets = new ArrayList<MatOfPoint>();
-        ArrayList<MatOfPoint> rightTargets = new ArrayList<MatOfPoint>();
+        // ArrayList<MatOfPoint> leftTargets = new ArrayList<MatOfPoint>();
+        // ArrayList<MatOfPoint> rightTargets = new ArrayList<MatOfPoint>();
 
         // Mat image = pipeline.hsvThresholdOutput();
         Mat image = new Mat(pipeline.hsvThresholdOutput().rows(), pipeline.hsvThresholdOutput().cols(), CvType.CV_8UC3);
 
         // draws contours in red and green, and adds convex hulls to left and right
         // ArrayLists
-        for (int i = 0; i < pipeline.convexHullsOutput().size(); i++) {
-            Tilt t = getHullTilt(pipeline.convexHullsOutput().get(i));
-            if (t == Tilt.Left) {
-                Imgproc.drawContours(image, pipeline.convexHullsOutput(), i, new Scalar(0, 0, 255), 3);
-                leftTargets.add(pipeline.convexHullsOutput().get(i));
-            } else {
-                Imgproc.drawContours(image, pipeline.convexHullsOutput(), i, new Scalar(0, 255, 0), 3);
-                rightTargets.add(pipeline.convexHullsOutput().get(i));
+        /*
+         * for (int i = 0; i < pipeline.convexHullsOutput().size(); i++) { Tilt t =
+         * getHullTilt(pipeline.convexHullsOutput().get(i)); if (t == Tilt.Left) {
+         * Imgproc.drawContours(image, pipeline.convexHullsOutput(), i, new Scalar(0, 0,
+         * 255), 3); leftTargets.add(pipeline.convexHullsOutput().get(i)); }else {
+         * Imgproc.drawContours(image, pipeline.convexHullsOutput(), i, new Scalar(0,
+         * 255, 0), 3); rightTargets.add(pipeline.convexHullsOutput().get(i)); }
+         * 
+         * }
+         */
+        int targetHolder = 0;
+        for (int i = 1; i < pipeline.convexHullsOutput().size(); i++) {
+            if (Imgproc.contourArea(pipeline.convexHullsOutput().get(i)) > Imgproc
+                    .contourArea(pipeline.convexHullsOutput().get(targetHolder))) {
+                targetHolder = i;
             }
         }
 
         // draws a blue rectangle arround paired targets
-        ArrayList<TargetPair> targetPairs = new ArrayList<TargetPair>();
-        for (int i = 0; i < leftTargets.size(); i++) {
-            TargetPair pair = new TargetPair(leftTargets.get(i), rightTargets);
-            if (pair.getHasPair()) {
-                targetPairs.add(pair);
-                Imgproc.rectangle(image, pair.topLeft(), pair.bottomRight(), new Scalar(255, 20, 10), -0);
-                // Imgproc.circle(image, pairCenter, 12, new Scalar(255, 6, 6));
-            }
-        }
+        /**
+         * ArrayList<TargetPair> targetPairs = new ArrayList<TargetPair>(); for (int i =
+         * 0; i < leftTargets.size(); i++) { TargetPair pair = new
+         * TargetPair(leftTargets.get(i), rightTargets); if (pair.getHasPair()) {
+         * targetPairs.add(pair); Imgproc.rectangle(image, pair.topLeft(),
+         * pair.bottomRight(), new Scalar(255, 20, 10), -0); // Imgproc.circle(image,
+         * pairCenter, 12, new Scalar(255, 6, 6)); } }
+         */
 
         // selects the "best pair" and draws a yellow rectangle
-        double bestPairWidth = 0;
-        if (targetPairs.size() > 0) {
-            TargetPair bestPair = targetPairs.get(0);
-            for (int i = 0; i < targetPairs.size(); ++i) {
-                if (targetPairs.get(i).pairSpread() < bestPair.pairSpread()) {
-                    bestPair = targetPairs.get(i);
-                }
-            }
-            Imgproc.rectangle(image, bestPair.topLeft(), bestPair.bottomRight(), new Scalar(0, 255, 255), 2);
-            // Sets angleX and angleY
-            angleX = findAngle(bestPair.findCenter().x, image.cols(), fovx);
-            angleY = findAngle(bestPair.findCenter().y, image.rows(), fovy);
-            bestPairWidth = bestPair.pairSpread();
-            distance = referenceTargetWidth * focalLength / bestPairWidth;
+        /*
+         * double bestPairWidth = 0; if (targetPairs.size() > 0) { TargetPair bestPair =
+         * targetPairs.get(0); for (int i = 0; i < targetPairs.size(); ++i) { if
+         * (targetPairs.get(i).pairSpread() < bestPair.pairSpread()) { bestPair =
+         * targetPairs.get(i); } }
+         */
 
-            if (pipeline.convexHullsOutput().size() > 0) {
-                confidence = 1;
-            } else {
-                confidence = 0;
-            }
-        }
-
+        /**
+         * Imgproc.rectangle(image, bestPair.topLeft(), bestPair.bottomRight(), new
+         * Scalar(0, 255, 255), 2); // Sets angleX and angleY angleX =
+         * findAngle(bestPair.findCenter().x, image.cols(), fovx); angleY =
+         * findAngle(bestPair.findCenter().y, image.rows(), fovy); bestPairWidth =
+         * bestPair.pairSpread(); distance = referenceTargetWidth * focalLength /
+         * bestPairWidth;
+         * 
+         * if (pipeline.convexHullsOutput().size() > 0) { confidence = 1; } else {
+         * confidence = 0; } }
+         */
         long timeSpan = System.currentTimeMillis() - previousTime;
         previousTime = System.currentTimeMillis();
         networkTable.getEntry(TARGET_FPS).setNumber(Math.round(1000.0 / timeSpan));
@@ -141,18 +147,18 @@ public class VisionTargetListener implements VisionRunner.Listener<VisionTargetP
         networkTable.getEntry(TARGET_ANGLE_Y).setNumber(angleY);
         networkTable.getEntry(TARGET_DISTANCE).setNumber(distance);
         networkTable.getEntry(TARGET_CONFIDENCE).setNumber(confidence);
-        networkTable.getEntry(TARGET_PAIRS).setNumber(targetPairs.size());
-        networkTable.getEntry(TARGET_WIDTH).setNumber(bestPairWidth);
+        // networkTable.getEntry(TARGET_PAIRS).setNumber(targetPairs.size());
+        networkTable.getEntry(TARGET_WIDTH).setNumber(contourWidth(pipeline.convexHullsOutput().get(targetHolder)));
         ntinst.flush();
         targetStream.putFrame(image);
 
-        long saveImageTime = ((Number)networkTable.getEntry(TARGET_SAVE).getNumber(Double.valueOf(-1.0))).longValue();
+        long saveImageTime = ((Number) networkTable.getEntry(TARGET_SAVE).getNumber(Double.valueOf(-1.0))).longValue();
         if (saveImageTime <= 0) {
             saveImageTimeout = 0;
         } else if (prevSaveImageTimeout <= 0) {
             saveImageFile(image);
             saveImageTimeout = System.currentTimeMillis() + saveImageTime;
-        } else if (System.currentTimeMillis() > saveImageTimeout)  {
+        } else if (System.currentTimeMillis() > saveImageTimeout) {
             saveImageFile(image);
             saveImageTimeout = System.currentTimeMillis() + saveImageTime;
         }
@@ -198,6 +204,12 @@ public class VisionTargetListener implements VisionRunner.Listener<VisionTargetP
         return center;
     }
 
+    // Finds width of convex hull
+    public static double contourWidth(MatOfPoint hull) {
+        Rect rectHolder = Imgproc.boundingRect(hull);
+        return rectHolder.width;
+    }
+
     // Find angle from pixel data
     public static double findAngle(double pixel, int resolution, int fov) {
         double center = pixel - (resolution / 2);
@@ -208,60 +220,36 @@ public class VisionTargetListener implements VisionRunner.Listener<VisionTargetP
         return out;
     }
 
-    private class TargetPair {
-        Point leftCenter;
-        Point rightCenter;
-        boolean hasPair = false;
-
-        // pairs right and left convex hulls
-        TargetPair(MatOfPoint left, ArrayList<MatOfPoint> rightMats) {
-            leftCenter = centerOfConvexHull(left);
-            Point bestFit = new Point(99999, 0);
-            for (int i = 0; i < rightMats.size(); i++) {
-                Point tmpRightCenter = centerOfConvexHull(rightMats.get(i));
-                if (tmpRightCenter.x > leftCenter.x && tmpRightCenter.x < bestFit.x) {
-                    bestFit = tmpRightCenter;
-                    hasPair = true;
-                }
-            }
-            rightCenter = bestFit;
-        }
-
-        public boolean getHasPair() {
-            return hasPair;
-        }
-
-        // finds the center point of a target pair
-        public Point findCenter() {
-            double averageX = (rightCenter.x + leftCenter.x) / 2;
-            double averageY = (rightCenter.y + leftCenter.y) / 2;
-            return new Point(averageX, averageY);
-        }
-
-        public Point topLeft() {
-            return new Point(leftCenter.x - 27, leftCenter.y - 37);
-        }
-
-        public Point bottomRight() {
-            return new Point(rightCenter.x + 27, rightCenter.y + 37);
-        }
-
-        public double pairSpread() {
-            return rightCenter.x - leftCenter.x;
-        }
-    }
-
-    enum Tilt {
-        Left, Right;
-    }
-
-    public static Tilt getHullTilt(MatOfPoint hull) {
-        Mat line = new Mat();
-        Imgproc.fitLine(hull, line, Imgproc.CV_DIST_L2, 0, 0.1, 0.1);
-        if (line.get(1, 0)[0] < 0.0) {
-            return Tilt.Left;
-        } else {
-            return Tilt.Right;
-        }
-    }
+    /**
+     * private class TargetPair { Point leftCenter; Point rightCenter; boolean
+     * hasPair = false;
+     * 
+     * // pairs right and left convex hulls TargetPair(MatOfPoint left,
+     * ArrayList<MatOfPoint> rightMats) { leftCenter = centerOfConvexHull(left);
+     * Point bestFit = new Point(99999, 0); for (int i = 0; i < rightMats.size();
+     * i++) { Point tmpRightCenter = centerOfConvexHull(rightMats.get(i)); if
+     * (tmpRightCenter.x > leftCenter.x && tmpRightCenter.x < bestFit.x) { bestFit =
+     * tmpRightCenter; hasPair = true; } } rightCenter = bestFit; }
+     * 
+     * public boolean getHasPair() { return hasPair; }
+     * 
+     * // finds the center point of a target pair public Point findCenter() { double
+     * averageX = (rightCenter.x + leftCenter.x) / 2; double averageY =
+     * (rightCenter.y + leftCenter.y) / 2; return new Point(averageX, averageY); }
+     * 
+     * public Point topLeft() { return new Point(leftCenter.x - 27, leftCenter.y -
+     * 37); }
+     * 
+     * public Point bottomRight() { return new Point(rightCenter.x + 27,
+     * rightCenter.y + 37); }
+     * 
+     * public double pairSpread() { return rightCenter.x - leftCenter.x; } }
+     */
+    /**
+     * enum Tilt { Left, Right; }
+     * 
+     * public static Tilt getHullTilt(MatOfPoint hull) { Mat line = new Mat();
+     * Imgproc.fitLine(hull, line, Imgproc.CV_DIST_L2, 0, 0.1, 0.1); if (line.get(1,
+     * 0)[0] < 0.0) { return Tilt.Left; } else { return Tilt.Right; } }\
+     */
 }
